41b3060776754e1d665af8dc60952238a8a49c17
Mike332149 <saqrmike332@gmail.com>
2024-02-28 16:06:00 +0200

    в activity_main.xml розширено rv_list за рахунок зменшення висоти інших елементів. в MainActivity.java додано класи RecyclerView,SavedItemsAdapter,SavedItem та інші.

--- app/src/main/java/com/example/lesson9androiditproger/MainActivity.java
+++ app/src/main/java/com/example/lesson9androiditproger/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lesson9androiditproger;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,29 +18,84 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 public class MainActivity extends AppCompatActivity {
-    private EditText user_name_field, user_bio_field;
+
+    public RecyclerView recyclerView;
+    private List<SavedItem> savedItemsList;
+    public SavedItemsAdapter adapter;
+
+    private EditText eventName_field, dateOrPassword_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user_name_field = findViewById(R.id.user_name_field);
-        user_bio_field = findViewById(R.id.user_bio_field);
+        eventName_field = findViewById(R.id.eventName_field);
+        dateOrPassword_field = findViewById(R.id.dateOrPassword_field);
+
+        // Инициализация данных (пример, заглушка)
+        savedItemsList = new ArrayList<>();
+
+        // Настройка RecyclerView
+        recyclerView = findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new SavedItemsAdapter(savedItemsList);
+        getData();
+        recyclerView.setAdapter(adapter);
     }
 
+    public class SavedItem {
+        private String eventName;
+        private String dateOrPassword;
+
+        public SavedItem(String eventName, String dateOrPassword) {
+            this.eventName = eventName;
+            this.dateOrPassword = dateOrPassword;
+        }
+        public String getEventName() {
+            return eventName;
+        }
+
+        public String getDateOrPassword() {
+            return dateOrPassword;
+        }
+    }
     public void saveData (View view){
-        String user_name = user_name_field.getText().toString();
-        String user_bio = user_bio_field.getText().toString();
+
+        String eventName = eventName_field.getText().toString();
+        String dateOrPassword = dateOrPassword_field.getText().toString();
+
+        // Додаємо новий елемент у список(!)який вже є в класі onCreate
+        savedItemsList.add(new SavedItem(eventName, dateOrPassword));
+
+        // Оновлюємо адаптер
+        adapter.notifyDataSetChanged();
 
         try {
-            FileOutputStream fileOutput = openFileOutput("user_data.txt",MODE_PRIVATE);
-            fileOutput.write((user_name + ". " + user_bio).getBytes());
+            FileOutputStream fileOutput = openFileOutput("user_data.txt", MODE_PRIVATE);
+            String s = "";
+            for (int i = 0; i < adapter.savedItems.size() ; i++) {
+                s += (adapter.savedItems.get(i).eventName + "." + adapter.savedItems.get(i).dateOrPassword) + "\n";
+            }
+
+            fileOutput.write(s.getBytes());
             fileOutput.close();
 
-            user_name_field.setText("");
-            user_bio_field.setText("");
+            eventName_field.setText("");
+            dateOrPassword_field.setText("");
             Toast.makeText(this, "Текст збережено", Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -45,19 +103,23 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-    }
-    public void getData (View view){
+        }
+        public void getData (){
         try {
             FileInputStream fileInput = openFileInput("user_data.txt");
             InputStreamReader reader = new InputStreamReader(fileInput);
             BufferedReader bR = new BufferedReader(reader);
-
-           StringBuilder stringBuilder = new StringBuilder();
-            String lines = "";
+           String lines = "";
            while ((lines = bR.readLine()) != null) {
-               stringBuilder.append(lines).append("\n");
+               if (lines.isEmpty()) {
+                   return;
+               }
+               String[] wordsepar = lines.split("\\.");
+               String evname = wordsepar[0];
+               String dpass = wordsepar[1];
+               SavedItem item = new SavedItem(evname, dpass);
+               adapter.savedItems.add(item);
            }
-           Toast.makeText(this, stringBuilder,Toast.LENGTH_LONG).show();
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -74,4 +136,45 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, WebActivity.class);
         startActivity(intent);
     }
+    public class SavedItemsAdapter extends RecyclerView.Adapter<SavedItemsAdapter.ViewHolder> {
+
+        private List<SavedItem> savedItems;
+
+        public SavedItemsAdapter(List<SavedItem> savedItems) {
+            this.savedItems = savedItems;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_saved_gpt, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            SavedItem item = savedItems.get(position);
+            holder.eventNameTextView.setText(item.getEventName());
+            holder.dateOrPasswordTextView.setText(item.getDateOrPassword());
+        }
+
+        @Override
+        public int getItemCount() {
+            return savedItems.size();
+        }
+
+        public  class ViewHolder extends RecyclerView.ViewHolder {
+            TextView eventNameTextView;
+            TextView dateOrPasswordTextView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                eventNameTextView = itemView.findViewById(R.id.eventNameTextView);
+                dateOrPasswordTextView = itemView.findViewById(R.id.dateOrPasswordTextView);
+            }
+        }
+    }
+
 }
+
+
